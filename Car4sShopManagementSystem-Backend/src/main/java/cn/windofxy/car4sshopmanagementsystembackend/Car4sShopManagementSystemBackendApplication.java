ac72c3320239b2f37d40c1cfa204f5541fb478bc
windofxy <1594251995@qq.com>
2025-09-02 13:54:17 +0800

    Initial Commit

---
+++ Car4sShopManagementSystem-Backend/src/main/java/cn/windofxy/car4sshopmanagementsystembackend/Car4sShopManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.windofxy.car4sshopmanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Car4sShopManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Car4sShopManagementSystemBackendApplication.class, args);
+    }
+
+}
---
+++ Car4sShopManagementSystem-Backend/src/test/java/cn/windofxy/car4sshopmanagementsystembackend/Car4sShopManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.windofxy.car4sshopmanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Car4sShopManagementSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
